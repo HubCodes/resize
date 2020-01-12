93f5e2f05d27d46e1b7d609a53149dcb8ba6de21
Taemin Cha <krlrhkstk12@gmail.com>
2020-01-12 19:22:29 +0900

    Change type RGBA

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,33 @@ import           Control.Monad.ST
 import           Debug.Trace
 import           Lib
 
+data RGBA =
+  RGBA Int Int Int Int
+
+instance Num RGBA where
+  (RGBA a b c d) + (RGBA e f g h) = RGBA (a + e) (b + f) (c + g) (d + h)
+  (RGBA a b c d) - (RGBA e f g h) = RGBA (a - e) (b - f) (c - g) (d - h)
+  (RGBA a b c d) * (RGBA e f g h) = RGBA (a * e) (b * f) (c * g) (d * h)
+  abs (RGBA a b c d) = RGBA (abs a) (abs b) (abs c) (abs d)
+  signum (RGBA a b c d) = RGBA (signum a) (signum b) (signum c) (signum d)
+  fromInteger x = RGBA (fromInteger x) (fromInteger x) (fromInteger x) (fromInteger x)
+  negate (RGBA a b c d) = RGBA (negate a) (negate b) (negate c) (negate d)
+
+divRGBA :: RGBA -> RGBA -> RGBA
+divRGBA (RGBA r1 g1 b1 a1) (RGBA r2 g2 b2 a2) = RGBA (r1 `div` r2) (g1 `div` g2) (b1 `div` b2) (a1 `div` a2)
+
+class RGBAConversion a where
+  toRGBA :: a -> RGBA
+  fromRGBA :: RGBA -> a
+
+instance RGBAConversion PixelRGB8 where
+  toRGBA (PixelRGB8 r g b) = RGBA (fromIntegral r) (fromIntegral g) (fromIntegral b) 255
+  fromRGBA (RGBA r g b _) = PixelRGB8 (fromIntegral r) (fromIntegral g) (fromIntegral b)
+
+instance RGBAConversion PixelRGBA8 where
+  toRGBA (PixelRGBA8 r g b a) = RGBA (fromIntegral r) (fromIntegral g) (fromIntegral b) (fromIntegral a)
+  fromRGBA (RGBA r g b a) = PixelRGBA8 (fromIntegral r) (fromIntegral g) (fromIntegral b) (fromIntegral a)
+
 instance Show DynamicImage where
   show (ImageY8 _)     = "ImageY8"
   show (ImageY16 _)    = "ImageY16"
@@ -24,19 +51,6 @@ instance Show DynamicImage where
   show (ImageYCbCr8 _) = "ImageYCbCr8"
   show (ImageCMYK8 _)  = "ImageCMYK8"
   show (ImageCMYK16 _) = "ImageCMYK16"
-
-rgba8To16 :: PixelRGBA8 -> PixelRGBA16
-rgba8To16 (PixelRGBA8 r g b a) = PixelRGBA16 (fromIntegral r) (fromIntegral g) (fromIntegral b) (fromIntegral a)
-
-rgba16To8 :: PixelRGBA16 -> PixelRGBA8
-rgba16To8 (PixelRGBA16 r g b a) = PixelRGBA8 (fromIntegral r) (fromIntegral g) (fromIntegral b) (fromIntegral a)
-
-addPixelRGBA16 :: PixelRGBA16 -> PixelRGBA16 -> PixelRGBA16
-addPixelRGBA16 (PixelRGBA16 r1 g1 b1 a1) (PixelRGBA16 r2 g2 b2 a2) = PixelRGBA16 (r1 + r2) (g1 + g2) (b1 + b2) (a1 + a2)
-
-divPixelRGBA16 :: PixelRGBA16 -> Pixel16 -> PixelRGBA16
-divPixelRGBA16 (PixelRGBA16 r g b a) factor =
-  PixelRGBA16 (r `div` factor) (g `div` factor) (b `div` factor) (a `div` factor)
 
 main :: IO ()
 main = do
@@ -59,8 +73,8 @@ resizeImage samplingFactor img@Image {..} =
             let xRange = [x * samplingFactor .. x * samplingFactor + samplingFactor - 1]
                 yRange = [y * samplingFactor .. y * samplingFactor + samplingFactor - 1]
                 targetPixelList = [pixelAt img i j | i <- xRange, j <- yRange]
-                sum = foldr (addPixelRGBA16 . rgba8To16) (PixelRGBA16 0 0 0 0) targetPixelList
-                resultPixel = sum `divPixelRGBA16` fromIntegral (samplingFactor ^ 2)
-            writePixel targetImg x y $ rgba16To8 resultPixel
+                sum = foldr ((+) . toRGBA) (RGBA 0 0 0 0) targetPixelList
+                resultPixel = sum `divRGBA` fromIntegral (samplingFactor ^ 2)
+            writePixel targetImg x y $ fromRGBA resultPixel
             go (x + 1) y
     go 0 0
